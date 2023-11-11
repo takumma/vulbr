e3b545d53a48ce0ae64bb6e946c47aec5e19aede
takumma <taku.3001@icloud.com>
2023-11-11 19:31:44 +0900

    add server with any status code

--- work/server.py
+++ work/server.py
@@ -1,13 +1,20 @@
 import http.server
 import socketserver
+import argparse
+
+parser = argparse.ArgumentParser(description='sample http server with args');
+
+parser.add_argument('statusCode', type=int, default=200, help='statu code')
+
+args = parser.parse_args()
 
 class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         print("Server started at localhost:" + str(PORT))
 
-        self.send_response(0)
+        self.send_response(args.statusCode)
         self.end_headers()
-        html = f"<html><head></head><body><p>this is status code 0 page!</p></body></html>"
+        html = f"<html><head></head><body><p>this is status code { args.statusCode } page!</p></body></html>"
         self.wfile.write(bytes(html, "utf8"))
         return
 
