c0e94914707849100f7db29b0934812dc3881767
takumma <taku.3001@icloud.com>
2023-11-11 18:12:27 +0900

    update

--- work/4-5.py
+++ work/4-5.py
@@ -5,15 +5,15 @@ class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         print("Server started at localhost:" + str(PORT))
 
-        self.send_response(-50)
+        self.send_response(0)
         self.end_headers()
-        html = f"<html><head></head><body><p>this is status code -50 page!</p></body></html>"
+        html = f"<html><head></head><body><p>this is status code 0 page!</p></body></html>"
         self.wfile.write(bytes(html, "utf8"))
         return
 
 handler = HttpRequestHandler
 
-PORT = 8889
+PORT = 8888
 server = socketserver.TCPServer(("", PORT), handler)
 
 server.serve_forever()
